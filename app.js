8cf2497085e8b6f0a0cb2d429ad80399b500ded5
artur <artur@hungrig.se>
2019-10-22 13:44:13 +0200

    add gitignore dummy data

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+
+app.use('/graphql', graphqlHTTP({
+	schema,
+	graphiql: true,
+}));
+
+app.listen(4000, () => {
+	console.log('now listening on port 4000');
+});
